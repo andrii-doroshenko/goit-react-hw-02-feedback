31aec05ef219f0eb9ae02a4a65d546ce84a6088f
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-04-26 20:53:53 +0300

    feature_feedback

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode basename="/goit-react-hw-02-feedback">
+  <React.StrictMode>
     <App />
   </React.StrictMode>
 );
